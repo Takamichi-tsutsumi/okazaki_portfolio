5af4e5d0b9583b007ccbf5e50a7d9277ff203878
lisaaoyama <lisaaoyama85@gmail.com>
2018-06-17 18:38:11 +0900

    Slide almost and latest and icons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 $(function() {
 
+  // var loop = setInterval(function() {
+  //     //li先頭要素のクローンを作成
+  //     var clone = $(".background-slide-loop li:first").clone(true);
+  //     //li先頭要素のマージントップにマイナスを指定しアニメーションさせる
+  //     $(".background-slide-loop li:first").animate({
+  //     marginRight : "-500px"
+  //     }, {
+  //     duration : 500,
+  //     complete : function() {
+  //         //処理完了時に先頭要素を削除
+  //         $(".background-slide-loop li:first").remove();
+  //         //クローンをliの最後に追加
+  //         clone.clone(true).insertAfter($(".background-slide-loop li:last"));
+  //     }
+  //     });
+  // }, 1500);
+
+  var offset = 18;
+  setInterval(function() {
+    $(".background-slide-loop li").css("transform", "translateX(-" + offset + "%)");
+    offset += 15;
+    if (offset > 100) {
+      var clone = $(".background-slide-loop li:first").clone(true);
+      clone.clone(true).insertAfter($(".background-slide-loop li:last"));
+// これをどこにいれればいいかわからない　消すとうまくいく
+      $(".background-slide-loop li:first").remove();
+    }
+  }, 2000);
 });
