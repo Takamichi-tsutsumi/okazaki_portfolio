4860696a4cda80b3c814186a8a30dd5d55dd3838
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-08-15 13:37:27 -0700

    Add youtube

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
-$(function() {
-  setTimeout(function() {
-    $('.slick').slick({
-      autoplay: true,
-      draggable: false,
-      autoplaySpeed: 2000,
-      arrows: false
-    });
-  }, 1000);
+$(function () {
+  if ($('.slick').length > 0) {
+    setTimeout(function () {
+      $('.slick').slick({
+        autoplay: true,
+        draggable: false,
+        autoplaySpeed: 2000,
+        arrows: false
+      });
+    }, 1000);
+  }
+  if ($('.js-modal-btn').length > 0) {
+    $(".js-modal-btn").modalVideo();
+  }
 });
+
